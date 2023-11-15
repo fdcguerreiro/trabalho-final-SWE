ba82aad8c97a46e12b717a8e0dfb95bdef6cf494
Filipe Guerreiro <filipedcguerreiro@gmail.com>
2023-11-15 13:11:28 +0000

    springboot and pom changes

---
+++ swe-demo-proj/src/test/java/com/example/swedemoproj/SweDemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swedemoproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweDemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
